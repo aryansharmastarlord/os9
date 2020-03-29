ced66306e20395e654807bf834721d9b722bd20f
aryansharmastarlord <31982717+aryansharmastarlord@users.noreply.github.com>
2020-03-29 21:03:22 +0530

    Update nos.c

--- nos.c
+++ nos.c
@@ -1,76 +1,171 @@
 #include<stdio.h>
-
 struct process
 {
-      char process_name; 
-      int arrival_time, burst_time, ct, waiting_time, turnaround_time, priority;
-      int status; 
-}
-process_queue[10];
+	int processID;
+	int burstTime;
+	int arrivalTime;
+	int priority;
+	int waitTime;
+};
+int total_time,burst_time=0;
+int total=-1,i=-1; 
 
-int limit; 
-void Arrival_Time_Sorting() 
+struct process queue[100],result[100],swap;
+
+int process_create()
 {
-      struct process temp;
-      int i, j;
-      for(i = 0; i < limit - 1; i++) 
-      {
-            for(j = i + 1; j < limit; j++) 
-            {
-                  if(process_queue[i].arrival_time > process_queue[j].arrival_time)
-                  {
-                        temp = process_queue[i];
-                        process_queue[i] = process_queue[j];
-                        process_queue[j] = temp;
-                  }
-            }
-      }
+	int n;
+	printf("enter the number of process you want to enter:");
+	scanf("%d",&n);
+	return n;
 }
+
+void execute()
+{
+	if(total>=0)
+	{
+		int wait,j;
+		
+		if(burst_time!=0 && queue[0].burstTime!=0)
+		{
+			queue[0].burstTime--;
+			burst_time--;
+			queue[0].priority++;
+			queue[0].arrivalTime=total_time+1;
+			total_time++;
+			
+			for(wait=1;wait<=total;wait++)
+			{
+				queue[wait].priority+=2;
+				queue[wait].waitTime=++queue[wait].waitTime;
+			}
+		}
+		
+		if(queue[0].burstTime==0)
+		{
+			i++;
+			result[i]=queue[0];
+			for(wait=0;wait<total;wait++)
+			{
+				queue[wait]=queue[wait+1];
+			}
+			total--;
+		}
+		
+		for(wait=0;wait<total;wait++)
+		{
+			for(j=0;j<total;j++)
+			{
+				if(queue[wait].priority<=queue[j].priority)
+				{
+					swap=queue[wait];
+					queue[wait]=queue[j];
+					queue[j]=swap;
+				}
+			}
+		}
+		if(queue[0].priority<=queue[1].priority && total>=1)
+		{
+			swap=queue[0];
+			for(wait=0;wait<total;wait++)
+			{
+				queue[wait]=queue[wait+1];
+			}
+			queue[total]=swap;
+		}
+	}
+}
+
+
 
 int main()
 {
-      int i, time = 0, burst_time = 0, largest; 
-      char c; 
-      float wait_time = 0, turnaround_time = 0, average_waiting_time, average_turnaround_time;
-      printf("\nEnter Total Number of Processes:\t"); //print total number of processes
-      scanf("%d", &limit); //taking input from user for total number of processes
-      for(i = 0, c = 'A'; i < limit; i++, c++)//for loop
-      {
-            process_queue[i].process_name = c;  
-            printf("\nEnter Details For Process[%C]:\n", process_queue[i].process_name); 
-            printf("Enter Arrival Time:\t"); 
-            scanf("%d", &process_queue[i].arrival_time ); //taking input of arrival time from user
-            printf("Enter Burst Time:\t");
-            scanf("%d", &process_queue[i].burst_time);
-            printf("Enter Priority:\t"); 
-            scanf("%d", &process_queue[i].priority);
-            process_queue[i].status = 0; 
-            burst_time = burst_time + process_queue[i].burst_time; 
-      }
-      Arrival_Time_Sorting();
-      process_queue[9].priority = -9999;
-      printf("\nProcess Name\tArrival Time\tBurst Time\tPriority\tWaiting Time");
-      for(time = process_queue[0].arrival_time; time < burst_time;)
-      {
-            largest = 9;
-            for(i = 0; i < limit; i++)
-            {
-                  if(process_queue[i].arrival_time <= time && process_queue[i].status != 1 && process_queue[i].priority > process_queue[largest].priority)
-                  {
-                        largest = i;
-                  }
-            }
-            time = time + process_queue[largest].burst_time; 
-            process_queue[largest].ct = time;
-            process_queue[largest].waiting_time = process_queue[largest].ct - process_queue[largest].arrival_time - process_queue[largest].burst_time; 
-            process_queue[largest].turnaround_time = process_queue[largest].ct - process_queue[largest].arrival_time; 
-            process_queue[largest].status = 1; 
-            wait_time = wait_time + process_queue[largest].waiting_time;
-            turnaround_time = turnaround_time + process_queue[largest].turnaround_time; //calculate turnaround time
-            printf("\n%c\t\t%d\t\t%d\t\t%d\t\t%d", process_queue[largest].process_name, process_queue[largest].arrival_time, process_queue[largest].burst_time, process_queue[largest].priority, process_queue[largest].waiting_time);
-      }
-      average_waiting_time = wait_time / limit; // calculate average waiting time
-      average_turnaround_time = turnaround_time / limit;
-      printf("\n\nAverage waiting time:\t%f\n", average_waiting_time); 
-      printf("Average Turnaround Time:\t%f\n", average_turnaround_time); // print our average turnaround time
+	int l,j,n=process_create(),count=0;
+	float averageWaitTime=0;
+	struct process pcreate[n];
+	for(l=0;l<n;l++)
+	{
+		pcreate[l].processID=l+1;
+		printf("\nEnter the arrival time of process[%d]: ",l+1);
+		scanf("%d",&pcreate[l].arrivalTime);
+		printf("\nEnter the burst time of process[%d]: ",l+1);
+		scanf("%d",&pcreate[l].burstTime);
+		pcreate[l].priority=0;
+		pcreate[l].waitTime=0;
+		burst_time=burst_time+pcreate[l].burstTime;
+	}
+	for(l=0;l<n;l++)
+	{
+		for(j=0;j<n;j++)
+		{
+		if(pcreate[l].arrivalTime<pcreate[j].arrivalTime)
+		{
+			swap=pcreate[l];
+			pcreate[l]=pcreate[j];
+			pcreate[j]=swap;
+		}
+		if(pcreate[l].arrivalTime==pcreate[j].arrivalTime)
+		{
+			if(pcreate[l].burstTime<=pcreate[j].burstTime)
+			{
+			swap=pcreate[l];
+			pcreate[l]=pcreate[j];
+			pcreate[j]=swap;
+			}
+	 	}
+	    }
 }
+	printf("VALUES ENTERED:\n*(TABLE SORTED ACCORDING TO THE AARIVAL TIME)\n\n");
+	printf("               PROCESS TABLE \n");
+	printf("\n.............................................\n");
+	printf(" PROCESS ID           ARRIVAL TIME        SERVICE TIME \n");
+	printf("\n.............................................\n");
+	for(l=0;l<n;l++)
+	{
+		printf(" %d            	   %d                   %d\n",pcreate[l].processID,pcreate[l].arrivalTime,pcreate[l].burstTime );
+	}
+	total_time=pcreate[0].arrivalTime;
+	for(j=pcreate[0].arrivalTime;j<=pcreate[n-1].arrivalTime;j++)
+	{
+		for(l=0;l<n;l++)
+		{
+			if(pcreate[l].arrivalTime==j && count!=n)
+			{
+				total++;
+				queue[total]=pcreate[l];
+				count++;
+			}
+			if(count==n)
+				break;
+		}
+		execute();
+		total_time++;
+	while(burst_time!=0 && count==n)
+	{
+		execute();
+		total_time++;
+	}
+	if(count==n)
+		break;
+	}
+	printf("PROCESS IN ORDER OF THEIR COMPLETION:\n\n");
+	printf("					FINAL PROCESS EXECUTION TABLE \n");
+	printf("................................................................................\n");
+	printf("    PROCESS ID          ARRIVAL TIME             SERVICE TIME          WAITING TIME\n");
+	printf("................................................................................\n");
+	for(l=0;l<n;l++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(result[l].processID==pcreate[j].processID)
+			{
+	printf("  	%d                  %d                    %d                    	%d\n",result[l].processID,pcreate[j].arrivalTime,pcreate[j].burstTime,result[l].waitTime);
+			break;
+			}
+		}
+		averageWaitTime+=(result[l].waitTime);
+	}
+	printf("AVERAGE  WAITING  TIME :%f\n",averageWaitTime/n);
+}	
+
+  
